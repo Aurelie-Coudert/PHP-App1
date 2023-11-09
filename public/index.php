8d83989b116e74899962ed638419eb5d5d2e3eff
Aurelie Coudert <aurelie.proffit@gmail.com>
2023-11-09 11:41:19 +0100

    système de router

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ require 'vendor/autoload.php';
 
 $router = new Router($_GET['url']);
 
-$router->get('/', 'BlogController@index');
-$router->get('/posts/:id', 'BlogController@show');
+$router->get('/', 'App\Controllers\BlogController@index');
+$router->get('/posts/:id', 'App\Controllers\BlogController@show');
 
 $router->run();
